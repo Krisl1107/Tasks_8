56cacb9ccfadbc824c56b20dbc925d671cddfc2f
Krisl1107 <Klagoda_zeya@mail.ru>
2025-04-07 10:01:28 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,4 +1,6 @@
+
 z=""
+
 for i in range(1,10):
         k=z+str(i)
         m=int(k)*8+i
@@ -10,3 +12,9 @@ for i in range(1,10):
         m=int(k)*9+i+1
         print(k,"* 9 + ",i+1,"=",m)
         z=k
+
+z="1"
+for i in range(1,10):
+        k=z*i
+        m=int(k)**2
+        print(k, "*  ",k, "=", m)
